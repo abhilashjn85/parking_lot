99c7edea8ceb0f6294b0f952c2594dd8be35e350
abhilash <abhilash.jain@oyorooms.com>
2020-01-03 11:08:50 +0530

    What: IssueSlotService, takes care of all the data and operartions around vehicle, slots etc.

--- src/main/java/com.gojek.parking/service/IssueSlotService.java
+++ src/main/java/com.gojek.parking/service/IssueSlotService.java
@@ -1,5 +1,24 @@
 package com.gojek.parking.service;
 
+import com.gojek.parking.api.Vehicle;
+
+import java.util.List;
+
 public interface IssueSlotService {
-    int fetchAvailableSlotNumber();
+
+    int parkCar(Vehicle vehicle);
+
+    Boolean leaveCar(int slotNumber);
+
+    List<String> fetchStatus();
+
+    List<String> fetchRegistrationNumberByColor(String color);
+
+    List<Integer> fetchSlotNumbersByColor(String colour);
+
+    int fetchSlotNumberByRegistrationNumber(String registrationNo);
+
+    int fetchAvailableSlot();
+
+    void refresh();
 }
--- src/main/java/com.gojek.parking/service/impl/IssueSlotServiceImpl.java
+++ src/main/java/com.gojek.parking/service/impl/IssueSlotServiceImpl.java
@@ -1,32 +1,141 @@
 package com.gojek.parking.service.impl;
 
-import com.gojek.parking.exception.ParkingLotException;
+import com.gojek.parking.api.Vehicle;
 import com.gojek.parking.service.IssueSlotService;
-import com.gojek.parking.util.ErrorCodes;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class IssueSlotServiceImpl implements IssueSlotService {
 
-    //TODO: issue with multithreading
-    private static Map<Integer, Boolean> slotCountStatusMap = new HashMap<>();
-
     public IssueSlotServiceImpl() {
 
-        //TODO: think of available slots and put in separate set.
+    }
+
+    private AtomicInteger capacity = new AtomicInteger();
+    private Map<Integer, String> slotVehicleRegistrationMap;
+    private TreeMap<Integer, Boolean> availableSlotsMap = new TreeMap<>();
+    private Map<String, Vehicle> vehicleRegistrationMap = new HashMap<>();
+
+    private static IssueSlotServiceImpl instance = null;
+
+    public static IssueSlotServiceImpl getInstance(int capacity) {
+        if (instance == null) {
+            synchronized (IssueSlotServiceImpl.class) {
+                if (null == instance) {
+                    instance = new IssueSlotServiceImpl(capacity);
+                }
+            }
+        }
+        return instance;
+    }
+
+    private IssueSlotServiceImpl(int capacity) {
+        this.capacity.set(capacity);
+        slotVehicleRegistrationMap = new ConcurrentHashMap<>();
+        for (int i = 1; i <= capacity; i++) {
+            slotVehicleRegistrationMap.put(i, null);
+            availableSlotsMap.put(i, Boolean.TRUE);
+        }
     }
 
     @Override
-    public int fetchAvailableSlotNumber() {
-        return 0;
+    public int parkCar(Vehicle vehicle)
+    {
+        if (availableSlotsMap.size() == 0) {
+            return -1;
+        } else if (slotVehicleRegistrationMap.containsValue(vehicle.getRegistrationNumber())) {
+            return -1;
+        }
+
+        int availableSlotFirstKey = availableSlotsMap.firstKey().intValue();
+        availableSlotsMap.remove(availableSlotFirstKey);
+        slotVehicleRegistrationMap.put(availableSlotFirstKey, vehicle.getRegistrationNumber());
+        vehicleRegistrationMap.put(vehicle.getRegistrationNumber(), vehicle);
+
+        return availableSlotFirstKey;
     }
 
-    public void commitSlot(Integer slotNumber) {
-
-        if(Boolean.FALSE == slotCountStatusMap.get(slotNumber)) {
-            slotCountStatusMap.put(slotNumber, Boolean.TRUE);
+    @Override
+    public Boolean leaveCar(int slotNumber)
+    {
+        if (null == slotVehicleRegistrationMap.get(slotNumber)) {
+            return false;
         }
-        throw new ParkingLotException(ErrorCodes.DUPLICATE_SLOT_FOUND, "Slot already allocated, Please try again.");
+
+        availableSlotsMap.remove(slotNumber);
+        slotVehicleRegistrationMap.put(slotNumber, null);
+        return true;
+    }
+
+    @Override
+    public List<String> fetchStatus() {
+        List<String> results = new ArrayList<>();
+        for (int i = 1; i <= capacity.get(); i++) {
+            String registrationKey = slotVehicleRegistrationMap.get(i);
+            Vehicle vehicle = vehicleRegistrationMap.get(registrationKey);
+            if (null != vehicle) {
+                results.add(i + "\t\t" + registrationKey + "\t\t" + vehicle.getColor());
+            }
+        }
+        return results;
+    }
+
+    @Override
+    public List<String> fetchRegistrationNumberByColor(String color) {
+        List<String> results = new ArrayList<>();
+        for (int i = 1; i <= capacity.get(); i++) {
+            String registrationKey = slotVehicleRegistrationMap.get(i);
+            Vehicle vehicle = vehicleRegistrationMap.get(registrationKey);
+            if (null != vehicle && color.equalsIgnoreCase(vehicle.getColor())) {
+                results.add(registrationKey);
+            }
+        }
+        return results;
+    }
+
+    @Override
+    public List<Integer> fetchSlotNumbersByColor(String colour) {
+        List<Integer> results = new ArrayList<>();
+        for (int i = 1; i <= capacity.get(); i++) {
+            String registrationKey = slotVehicleRegistrationMap.get(i);
+            Vehicle vehicle = vehicleRegistrationMap.get(registrationKey);
+
+            if (null != vehicle && colour.equalsIgnoreCase(vehicle.getColor())) {
+                results.add(i);
+            }
+        }
+        return results;
+    }
+
+    @Override
+    public int fetchSlotNumberByRegistrationNumber(String registrationNumber) {
+
+        int result = -1;
+        for (int i = 1; i <= capacity.get(); i++) {
+            String registrationKey = slotVehicleRegistrationMap.get(i);
+            Vehicle vehicle = vehicleRegistrationMap.get(registrationKey);
+            if (null != vehicle && registrationNumber.equalsIgnoreCase(registrationKey)) {
+                result = i;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public int fetchAvailableSlot() {
+        return availableSlotsMap.firstKey();
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException();
+    }
+
+    @Override
+    public void refresh() {
+        this.capacity = new AtomicInteger();
+        slotVehicleRegistrationMap = new HashMap<>();
+        vehicleRegistrationMap = new HashMap<>();
+        instance = null;
     }
 }
